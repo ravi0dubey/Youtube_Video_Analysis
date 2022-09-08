3d4539a49150922da77eff914bc4f37d89e1fee5
ravi0dubey <ravi0dubey@gmail.com>
2022-09-08 06:23:36 -0400

    fetching Videos statistics for each channel

--- Youtube_Statstistic.py
+++ Youtube_Statstistic.py
@@ -11,17 +11,72 @@ class YTstats:
 
 
     def get_channel_statistics(self):
-        all_data = []
-        youtube = build('youtube', 'v3', developerKey=self.api_key)
-        request = youtube.channels().list(part='snippet,contentDetails,statistics',id=','.join(self.channel_id))
-        response = request.execute()
-        print(response)
-        for i in range(len(response['items'])):
-            data = dict(Channel_name=response['items'][i]['snippet']['title'],
-                        Subscribers=response['items'][i]['statistics']['subscriberCount'],
-                        Views=response['items'][i]['statistics']['viewCount'],
-                        Total_Videos=response['items'][i]['statistics']['videoCount'],
-                        playlist_ID=response['items'][i]['contentDetails']['relatedPlaylists']['uploads'])
-            all_data.append(data)
+        self.all_data = []
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.request = self.youtube.channels().list(part='snippet,contentDetails,statistics',id=','.join(self.channel_id))
+        self.response = self.request.execute()
 
-        return all_data
+        for i in range(len(self.response['items'])):
+            data = dict(Channel_name=self.response['items'][i]['snippet']['title'],
+                        Subscribers=self.response['items'][i]['statistics']['subscriberCount'],
+                        Views=self.response['items'][i]['statistics']['viewCount'],
+                        Total_Videos=self.response['items'][i]['statistics']['videoCount'],
+                        playlist_ID=self.response['items'][i]['contentDetails']['relatedPlaylists']['uploads'])
+            self.all_data.append(data)
+
+        return self.all_data
+
+# function to get videosid
+
+    def get_videos_id(self,playlist_ID):
+        self.request1= self.youtube.playlistItems().list(part='contentDetails',playlistId=playlist_ID, maxResults=50)
+        self.response1=self.request1.execute()
+        video_ids = []
+
+        for i in range(len(self.response1['items'])):
+            video_ids.append(self.response1['items'][i]['contentDetails']['videoId'])
+
+        self.next_page_token = self.response1.get('nextPageToken')
+        self.more_pages = True
+
+        while self.more_pages:
+            if self.next_page_token  is None:
+                self.more_pages = False
+            else:
+                self.request2 = self.youtube.playlistItems().list(
+                    part='contentDetails',
+                    playlistId=playlist_ID,
+                    maxResults=50,
+                    pageToken=self.next_page_token)
+                self.response2 = self.request2.execute()
+
+            for i in range(len(self.response2['items'])):
+                video_ids.append(self.response2['items'][i]['contentDetails']['videoId'])
+
+            next_page_token = self.response2.get('nextPageToken')
+
+        return video_ids
+
+
+## Function to get video details
+
+    def get_video_details(self, video_ids):
+        all_video_stats = []
+
+        for i in range(0, len(video_ids), 50):
+            request = self.youtube.videos().list(
+                part='snippet,statistics',
+                id=','.join(video_ids[i:i + 50]))
+            response = request.execute()
+
+            for video in response['items']:
+                video_stats = dict(Title=video['snippet']['title'],
+                                   Published_date=video['snippet']['publishedAt'],
+                                   Views=video['statistics']['viewCount'],
+                                   Likes=video['statistics']['likeCount'],
+                                   Dislikes=video['statistics']['dislikeCount'],
+                                   Comments=video['statistics']['commentCount']
+                                   )
+                all_video_stats.append(video_stats)
+
+        return all_video_stats
--- main.py
+++ main.py
@@ -17,17 +17,46 @@ channel_data = pd.DataFrame(channel_statistics)
 channel_data['Subscribers']= pd.to_numeric(channel_data['Subscribers'])
 channel_data['Views']= pd.to_numeric(channel_data['Views'])
 channel_data['Total_Videos']= pd.to_numeric(channel_data['Total_Videos'])
-print(channel_data)
+
 # x = channel_data['Channel_name']
 # y = channel_data['Subscribers']
 # sns.set(rc={'figure.figsize':(10,8)})
 ax_subscribers_count= sns.barplot(x='Channel_name',y='Subscribers',data= channel_data)
-# plt.bar(x,y)
-plt.show()
-
+# plt.show()
 ax_views_count = sns.barplot(x='Channel_name',y='Views',data= channel_data)
+# plt.show()
+ax_views_count = sns.barplot(x='Channel_name',y='Total_Videos',data= channel_data)
+# plt.show()
+
+#get playlist id of particular User
+playlist_id = channel_data.loc[channel_data['Channel_name']=='Ken Jee','playlist_ID'].iloc[0]
+
+#get all Videos_id of the Particular User
+videos_id = yt.get_videos_id(playlist_id)
+
+# Get all videos details of Particular User
+video_details = yt.get_video_details(videos_id)
+
+video_data = pd.DataFrame(video_details)
+
+video_data['Published_date'] = pd.to_datetime(video_data['Published_date']).dt.date
+video_data['Views'] = pd.to_numeric(video_data['Views'])
+video_data['Likes'] = pd.to_numeric(video_data['Likes'])
+video_data['Dislikes'] = pd.to_numeric(video_data['Dislikes'])
+video_data['Views'] = pd.to_numeric(video_data['Views'])
+video_data['Month'] = pd.to_datetime(video_data['Published_date']).dt.strftime('%b')
+top10_videos = video_data.sort_values(by='Views', ascending=False).head(10)
+print(top10_videos)
+
+ax_top_10_videos = sns.barplot(x='Views', y='Title', data=top10_videos)
 plt.show()
 
-ax_views_count = sns.barplot(x='Channel_name',y='Total_Videos',data= channel_data)
+videos_per_month = video_data.groupby('Month', as_index=False).size()
+sort_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+videos_per_month.index = pd.CategoricalIndex(videos_per_month['Month'], categories=sort_order, ordered=True)
+videos_per_month = videos_per_month.sort_index()
+ax_permonth_videso = sns.barplot(x='Month', y='size', data=videos_per_month)
 plt.show()
+
+
 
