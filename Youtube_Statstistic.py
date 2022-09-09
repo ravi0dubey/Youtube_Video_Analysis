29c2f631affcba553b4934666efe92a4feb45382
ravi0dubey <ravi0dubey@gmail.com>
2022-09-09 07:45:44 -0400

    fetching Videos statistics for each channel

--- Youtube_Statstistic.py
+++ Youtube_Statstistic.py
@@ -3,12 +3,10 @@ import json
 from googleapiclient.discovery import  build
 
 class YTstats:
-
     def __init__(self,api_key,channel_id):
         self.api_key= api_key
         self.channel_id = channel_id
         self.channel_statistics = None
-
 
     def get_channel_statistics(self):
         self.all_data = []
@@ -27,18 +25,14 @@ class YTstats:
         return self.all_data
 
 # function to get videosid
-
     def get_videos_id(self,playlist_ID):
         self.request1= self.youtube.playlistItems().list(part='contentDetails',playlistId=playlist_ID, maxResults=50)
         self.response1=self.request1.execute()
         video_ids = []
-
         for i in range(len(self.response1['items'])):
             video_ids.append(self.response1['items'][i]['contentDetails']['videoId'])
-
         self.next_page_token = self.response1.get('nextPageToken')
         self.more_pages = True
-
         while self.more_pages:
             if self.next_page_token  is None:
                 self.more_pages = False
@@ -53,30 +47,52 @@ class YTstats:
             for i in range(len(self.response2['items'])):
                 video_ids.append(self.response2['items'][i]['contentDetails']['videoId'])
 
-            next_page_token = self.response2.get('nextPageToken')
-
+            self.next_page_token  = self.response2.get('nextPageToken')
         return video_ids
 
 
-## Function to get video details
-
+# Function to get each videos Statistics details
     def get_video_details(self, video_ids):
         all_video_stats = []
-
         for i in range(0, len(video_ids), 50):
             request = self.youtube.videos().list(
                 part='snippet,statistics',
                 id=','.join(video_ids[i:i + 50]))
             response = request.execute()
-
+            print(response)
             for video in response['items']:
                 video_stats = dict(Title=video['snippet']['title'],
                                    Published_date=video['snippet']['publishedAt'],
                                    Views=video['statistics']['viewCount'],
                                    Likes=video['statistics']['likeCount'],
-                                   Dislikes=video['statistics']['dislikeCount'],
+                                   # Dislikes=video['statistics']['dislikeCount'],
                                    Comments=video['statistics']['commentCount']
                                    )
                 all_video_stats.append(video_stats)
-
         return all_video_stats
+
+
+# Function to get each videos comments
+#     def get_video_comments(self, video_ids):
+        all_video_comments = []
+        # for i in range(0, len(video_ids), 50):
+        #     request = self.youtube.commentThreads().list(
+        #         part='snippet,replies',
+        #         id=','.join(video_ids[i:i + 50]))
+        #     response = request.execute()
+        #     print(response)
+
+    def get_video_comments(self):
+        all_video_comments = []
+        request = self.youtube.commentThreads().list(
+            part="snippet",
+            videoId="EBjYqC3aNTA")
+        response = request.execute()
+        for item in response['items']:
+            comment = item["snippet"]["topLevelComment"]
+            video_stats = dict(
+                               author = comment["snippet"]["authorDisplayName"],
+                               comment_text = comment["snippet"]["textDisplay"]
+                               )
+            all_video_comments.append(video_stats)
+        return all_video_comments
